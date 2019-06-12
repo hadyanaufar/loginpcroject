fd366320f9a512f7f0f17291bb31e880b5fe1495
hadyan.aufar@live.com <hadyan.aufar@live.com>
2019-06-12 18:11:26 +0700

    stage 1

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
+    </div>
+    <router-view @authenticated="setAuthenticated" />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+      return {
+        authenticated: false,
+        mockAccount: {
+          username: "nraboy",
+          password: "password"
+        }
+      }
+    },
+    mounted() {
+      if(!this.authenticated) {
+        this.$router.replace({ name: "login" });
+      }
+    }
+}
+</script>
---
+++ src/views/secure.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="secure">
+        <h1>Secure Area</h1>
+        <p>
+            This is a secure Area
+        </p>
+    </div>
+</template>
+
+<<script>
+    export default {
+        name: 'Secure',
+        data() {
+            return {};
+        }
+    }
+</script>
+
+<style scoped>
+    #secure {
+        background-color: aquamarine;
+        border: 1px solid #cccccc;
+        padding: 20px;
+        margin-top: 10px;
+    }
+</style>
